1eb3e44a515b3ac794da959d6b4019c33683f51f
Sanaya James <sanaya.j.james@gmail.com>
2026-04-21 14:35:03 -0700

    established a relationship between employees and meetings using a join table

--- server/models.py
+++ server/models.py
@@ -10,6 +10,15 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# Association table to store many-to-many relationship between employees and meetings
+employee_meetings = db.Table(
+    'employees_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey(
+        'employees.id'), primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey(
+        'meetings.id'), primary_key=True)
+)
 
 class Employee(db.Model):
     __tablename__ = 'employees'
@@ -17,6 +26,9 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+
+    # Relationship mapping the employee to related meetings
+    meetings = db.relationship('Meeting', secondary=employee_meetings, back_populates='employees')
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
@@ -29,6 +41,9 @@ class Meeting(db.Model):
     topic = db.Column(db.String)
     scheduled_time = db.Column(db.DateTime)
     location = db.Column(db.String)
+
+    # Relationship mapping the meeting to related employees
+    employees = db.relationship('Employee', secondary=employee_meetings, back_populates='meetings')
 
     def __repr__(self):
         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
